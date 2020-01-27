eb1b4fe5b4e731d7b61818834970e4e153e0cf3e
Maxwell Brown <maxwellbrown1990@gmail.com>
2020-01-27 13:04:13 -0500

    Scaffold basic app layout

---
+++ packages/app/src/components/App/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import { Layout } from '../Layout';
+
+interface Props {}
+
+const fontStack = (fonts: string[]): string => {
+  return fonts.map(font => (font.includes(' ') ? `"${font}"` : font)).join(', ');
+};
+
+export const App: React.FC<Props> = ({ children }) => {
+  return (
+    <ThemeProvider
+      theme={createMuiTheme({
+        palette: {
+          primary: {
+            main: '#1976d2',
+          },
+          secondary: {
+            main: '#2196f3',
+          },
+        },
+        typography: {
+          fontFamily: fontStack([
+            'Nunito',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            'Segoe UI',
+            'Roboto',
+            'Helvetica Neue ',
+            'Arial',
+            'sans-serif',
+            'Apple Color Emoji',
+            'Segoe UI Emoji',
+            'Segoe UI Symbol',
+          ]),
+          h1: {
+            fontSize: '2rem',
+            fontWeight: 600,
+            lineHeight: 1.2,
+          },
+          h2: {
+            fontSize: '1.75rem',
+            fontWeight: 600,
+            lineHeight: 1.2,
+          },
+          h3: {
+            fontSize: '1.5rem',
+            fontWeight: 600,
+            lineHeight: 1.2,
+          },
+          h4: {
+            fontSize: '1.25rem',
+            fontWeight: 600,
+            lineHeight: 1.2,
+          },
+          h5: {
+            fontSize: '1.125rem',
+            fontWeight: 600,
+            lineHeight: 1.2,
+          },
+          h6: {
+            fontSize: '1.0125rem',
+            fontWeight: 600,
+            lineHeight: 1.2,
+          },
+        },
+      })}
+    >
+      <CssBaseline />
+      <Layout>{children}</Layout>
+    </ThemeProvider>
+  );
+};
